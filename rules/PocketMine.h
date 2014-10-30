e26ba96ad1463b1f73c7f1e717020e3aeedec52f
Shoghi Cervantes <shoghicp@gmail.com>
2014-10-30 19:22:32 +0100

    Added Level index optimizations

--- rules/PocketMine.h
+++ rules/PocketMine.h
@@ -3,3 +3,6 @@
 #include <rules/Vector3.h>
 
 #define COMPILE 1
+
+#define Level::chunkHash(chunkX, chunkZ) (chunkX . ":" . chunkZ)
+#define Level::getXZ(hash, chunkX, chunkZ) list(chunkX, chunkZ) = explode(":", hash); chunkX = (int) chunkX; chunkZ = (int) chunkZ;
