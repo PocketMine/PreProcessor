cbaef6c469bd44bd13b2be092275205f015df3bc
Shoghi Cervantes <shoghicp@gmail.com>
2014-10-30 14:15:29 +0100

    Fixed constant optimization

--- CodeOptimizer.php
+++ CodeOptimizer.php
@@ -34,7 +34,7 @@ function fastOptimize(array &$tree){
 	foreach($tree as $index => &$token){
 		if(is_array($token)){
 			if($token[0] === T_STRING and ($before = getBefore($tree, $index)) !== "\\"){
-				if(isset($constantOptimize[strtoupper($token[1])])){
+				if(isset($constantOptimize[strtoupper($token[1])]) and $before !== "::" and $before !== "->"){
 					$token[1] = "\\" . $token[1]; //Constant fetch optimization
 				}elseif(function_exists($token[1]) and $before !== "::" and $before !== "->" and $before !== "function" and getAfter($tree, $index) === "("){
 					$token[1] = "\\" . $token[1]; //Function call optimization
