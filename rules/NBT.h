d072ca1921b271f6e065bf06cecc3c2a3fce147f
Shoghi Cervantes <shoghicp@gmail.com>
2014-10-31 19:13:04 +0100

    Fixed errors

--- rules/NBT.h
+++ rules/NBT.h
@@ -1,7 +1,5 @@
 use pocketmine\utils\Binary;
 
-#include <rules/BinaryIO.h>
-
 #define $nbt->put(data) $nbt->buffer .= data
 
 #define $nbt->getLong() $nbt->endianness === 1 ? Binary::readLong($nbt->get(8)) : Binary::readLLong($nbt->get(8))
@@ -22,3 +20,24 @@ use pocketmine\utils\Binary;
 
 #define $nbt->getByte() ord($nbt->get(1))
 #define $nbt->putByte(data) $nbt->buffer .= chr(data)
+
+#define $this->put(data) $this->buffer .= data
+
+#define $this->getLong() $this->endianness === 1 ? Binary::readLong($this->get(8)) : Binary::readLLong($this->get(8))
+#define $this->putLong(data) $this->buffer .= $this->endianness === 1 ? Binary::writeLong(data) : Binary::writeLLong(data)
+
+#define $this->getInt() $this->endianness === 1 ? Binary::readInt($this->get(4)) : Binary::readLInt($this->get(4))
+#define $this->putInt(data) $this->buffer .= $this->endianness === 1 ? Binary::writeInt(data) : Binary::writeLInt(data)
+
+#define $this->getShort() $this->endianness === 1 ? Binary::readShort($this->get(2)) : Binary::readLShort($this->get(2))
+#define $this->getSignedShort() $this->endianness === 1 ? Binary::readSignedShort($this->get(2)) : Binary::readSignedLShort($this->get(2))
+#define $this->putShort(data) $this->buffer .= $this->endianness === 1 ? Binary::writeShort(data) : Binary::writeLShort(data)
+
+#define $this->getFloat() $this->endianness === 1 ? Binary::readFloat($this->get(4)) : Binary::readLFloat($this->get(4))
+#define $this->putFloat(data) $this->buffer .= $this->endianness === 1 ? Binary::writeFloat(data) : Binary::writeLFloat(data)
+
+#define $this->getDouble() $this->endianness === 1 ? Binary::readDouble($this->get(8)) : Binary::readLDouble($this->get(8))
+#define $this->putDouble(data) $this->buffer .= $this->endianness === 1 ? Binary::writeDouble(data) : Binary::writeLDouble(data)
+
+#define $this->getByte() ord($this->get(1))
+#define $this->putByte(data) $this->buffer .= chr(data)
