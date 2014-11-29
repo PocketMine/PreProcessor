f1539203475f01ccef3313dc4306f3580385d019
Shoghi Cervantes <shoghicp@gmail.com>
2014-11-29 11:57:51 +0100

    New awesome macro optimizations

--- rules/Level.h
+++ rules/Level.h
@@ -1,2 +1,14 @@
 
 #define $this->getChunkEntities(chunkX, chunkZ) (($______chunk = $this->getChunk(chunkX, chunkZ)) !== null ? $______chunk->getEntities() : [])
+
+#define $this->getChunk(chunkX, chunkZ, create) isset($this->chunks[$___________index = Level::chunkHash(chunkX, chunkZ)]) ? $this->chunks[$___________index] : (($this->loadChunk(chunkX, chunkZ, create) and $this->chunks[$___________index] instanceof FullChunk) ? $this->chunks[$___________index] : null)
+
+#define $this->isChunkLoaded(chunkX, chunkZ) isset($this->chunks[Level::chunkHash(chunkX, chunkZ)]) or $this->provider->isChunkLoaded(chunkX, chunkZ)
+
+#define $this->getBlockIdAt(x, y, z) $this->getChunk(x >> 4, z >> 4, true)->getBlockId(x & 0x0f, y & 0x7f, z & 0x0f)
+#define $this->getBlockDataAt(x, y, z) $this->getChunk(x >> 4, z >> 4, true)->getBlockData(x & 0x0f, y & 0x7f, z & 0x0f)
+
+#define $this->getBlockLightAt(x, y, z) $this->getChunk(x >> 4, z >> 4, true)->getBlockLight(x & 0x0f, y & 0x7f, z & 0x0f)
+#define $this->setBlockLightAt(x, y, z, level) $this->getChunk(x >> 4, z >> 4, true)->setBlockLight(x & 0x0f, y & 0x7f, z & 0x0f, level & 0x0f)
+#define $this->getBlockSkyLightAt(x, y, z) $this->getChunk(x >> 4, z >> 4, true)->getBlockSkyLight(x & 0x0f, y & 0x7f, z & 0x0f)
+#define $this->setBlockSkyLightAt(x, y, z, level) $this->getChunk(x >> 4, z >> 4, true)->setBlockSkyLight(x & 0x0f, y & 0x7f, z & 0x0f, level & 0x0f)
--- rules/PocketMine.h
+++ rules/PocketMine.h
@@ -3,5 +3,5 @@
 
 #define COMPILE 1
 
-#define Level::chunkHash(chunkX, chunkZ) (chunkX . ":" . chunkZ)
+#define Level::chunkHash(chunkX, chunkZ) ((chunkX) . ":" . (chunkZ))
 #define Level::getXZ(hash, chunkX, chunkZ) list(chunkX, chunkZ) = explode(":", hash); chunkX = (int) chunkX; chunkZ = (int) chunkZ;
