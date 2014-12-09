295f935320ebb75e9da8e914ae2c9e2431341afc
Shoghi Cervantes <shoghicp@gmail.com>
2014-12-09 15:03:33 +0100

    Updated rules

--- rules/PocketMine.h
+++ rules/PocketMine.h
@@ -3,5 +3,6 @@
 
 #define COMPILE 1
 
-#define Level::chunkHash(chunkX, chunkZ) ((chunkX) . ":" . (chunkZ))
-#define Level::getXZ(hash, chunkX, chunkZ) list(chunkX, chunkZ) = explode(":", hash); chunkX = (int) chunkX; chunkZ = (int) chunkZ;
+#define Level::chunkHash(chunkX, chunkZ) PHP_INT_SIZE === 8 ? (((chunkX) & 0xFFFFFFFF) << 32) | ((chunkZ) & 0xFFFFFFFF) : (chunkX) . ":" . (chunkZ)
+#define Level::blockHash(x, y, z) PHP_INT_SIZE === 8 ? (((x) & 0xFFFFFFF) << 35) | (((y) & 0x7f) << 28) | ((z) & 0xFFFFFFF) : (x) . ":" . (y) .":". (z)
+#define Level::getXZ(hash, chunkX, chunkZ) if(PHP_INT_SIZE === 8){chunkX = (hash >> 32) << 32 >> 32; chunkZ = (hash & 0xFFFFFFFF) << 32 >> 32;}else{list(chunkX, chunkZ) = explode(":", hash); chunkX = (int) chunkX; chunkZ = (int) chunkZ;}
